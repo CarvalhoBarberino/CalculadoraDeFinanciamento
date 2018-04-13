40f23b88294c483d02352742d0956bf2991f42db
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-04-13 19:50:50 -0300

    Upload Inicial
    Upload Inicial

---
+++ Calculadora de Financiamento/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame j0 = new JFrame("Calculadora de financiamento");
+		j0.setBounds(150, 150, 600, 400); j0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);j0.setResizable(false);j0.setVisible(true);
+		j0.getContentPane().setLayout(null);
+		JLabel jLNumeroDeParcelas = new JLabel("Digite o numero de parcelas");
+		JTextField JTNumeroDeParcelas = new JTextField(40);
+		
+		
+		//compra de whey com denis 28/03/2018 235,08 = 0,9 * (2 * 85 + 25,20 + 66)
+	}
+}
