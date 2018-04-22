1b441adb0ebafe20af339a590bd3c985c6ac4ccd
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-04-22 02:25:00 -0300

    Add files via upload

--- Calculadora de Financiamento/src/Main.java
+++ Calculadora de Financiamento/src/Main.java
@@ -1,9 +1,13 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,21 +16,24 @@ import javax.swing.JTextField;
 public class Main {
 	public static void main(String[] args) {
 		JFrame j0 = new JFrame("Calculadora de financiamento");
-		j0.setBounds(150, 150, 600, 400); j0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);j0.setResizable(true);j0.setVisible(true);
+		j0.setBounds(150, 150, 400, 400); j0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);j0.setResizable(true);
 		j0.getContentPane().setLayout(new FlowLayout());
-		JLabel jLNumeroDeParcelas = new JLabel("Digite o numero maximo de parcelas             ");
+		JLabel jLNumeroDeParcelas = new JLabel("Digite o numero maximo de parcelas     ");
 		JTextField jTNumeroDeParcelas = new JTextField(10);
 		JLabel jLJuros = new JLabel("Digite o valor do juros em porcentagem");
 		JTextField jTJuros = new JTextField(10);
-		JLabel jLValor = new JLabel("Digite o valor A ser fianciado                  ");
+		JLabel jLValor = new JLabel("Digite o valor a ser fianciado                   ");
 		JTextField jTValor = new JTextField(10);
 		Dimension dimLinha = new Dimension(400, 30);
 		JPanel pLinha1 = new JPanel();pLinha1.setPreferredSize(dimLinha); pLinha1.setLayout(new FlowLayout());pLinha1.add(jLValor); pLinha1.add(jTValor);j0.getContentPane().add(pLinha1);
 		JPanel pLinha2 = new JPanel();pLinha2.setPreferredSize(dimLinha);pLinha2.setLayout(new FlowLayout());pLinha2.add(jLNumeroDeParcelas); pLinha2.add(jTNumeroDeParcelas);j0.getContentPane().add(pLinha2);
 		JPanel pLinha3 = new JPanel();pLinha2.setPreferredSize(dimLinha);pLinha3.setLayout(new FlowLayout());pLinha3.add(jLJuros); pLinha3.add(jTJuros);j0.getContentPane().add(pLinha3);
-		System.out.println(Math.pow(3, 2));
 		JButton bCalcular = new JButton("Calcular");
 		j0.getContentPane().add(bCalcular);
+		JComboBox combo = new JComboBox();
+		j0.getContentPane().add(combo);
+		
+		j0.setVisible(true);
 		bCalcular.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
@@ -36,14 +43,23 @@ public class Main {
 					int nParcelas;
 					valorMontante = Float.valueOf(jTValor.getText());
 					nParcelas = Integer.valueOf(jTNumeroDeParcelas.getText());
-					juros = Float.valueOf(jTJuros.getText());
+					String[] lista = new String[nParcelas];
+					juros = Float.valueOf(jTJuros.getText())/100;
 					Problema problema1 = new Problema();
 					problema1.setValores(valorMontante, juros);
-					problema1.solve(nParcelas);
+					lista = problema1.solve(nParcelas);
+					for(int i = 0; i < nParcelas; i++){
+						combo.addItem(lista[i]);
+					}
 				}catch(Exception err){
 					err.printStackTrace();
 				}
 				
+			}
+		});
+		combo.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				System.out.println("chenge");
 			}
 		});
 		
--- Calculadora de Financiamento/src/Problema.java
+++ Calculadora de Financiamento/src/Problema.java
@@ -8,13 +8,14 @@ public class Problema {
 		valorMontante = valorMontanteArg;
 		juros = jurosArg;
 	}
-	public float solve(int numeroMaximoDeParcelas){
+	public String[] solve(int numeroMaximoDeParcelas){
 		float p;
+		String[] lista = new String[numeroMaximoDeParcelas];
 		for(int i = 1; i <= numeroMaximoDeParcelas; i++){
 			// esta é a notação definida no arquivo matematica: p = (Q * (1+j)^nP) / (1 + ((1+j)^nP - (1+j)) / j)
 			p = (valorMontante * (float)Math.pow(1+juros, i)) / (1 + ((float)Math.pow(1+juros, i) - (1+juros)) / juros);
-			System.out.println(i + "    " + p);
+			lista[i-1] = i + "    " + p;
 		}
-		return 0;
+		return lista;
 	}
 }
